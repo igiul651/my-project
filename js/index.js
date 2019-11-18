0803c6626ced9460693881a96304b58bf1e4f5a4
igiul651 <haydarluigi@gmail.com>
2019-11-18 01:15:20 +0000

    Home Work

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    var uluru = {lat: 49.800, lng: 24.012};
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 14, center: uluru,});
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({position: uluru, map: map});
+  }
